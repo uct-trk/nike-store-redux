e8a595b2d8d4ca34df821ab1f18ddeb71fb5c460
unknown <ugurcan.uut82@gmail.com>
2022-11-03 19:07:00 +0300

    nike store schema

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { Hero } from "./components";
-import { heroapi } from "./data/data";
+import { Hero, Sales, FlexContent, Stories, Footer } from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data";
 
 const App = () => {
   return (
     <>
-      <main>
+      <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
       </main>
+      <Footer footerAPI={footerAPI} />
     </>
   );
 };
